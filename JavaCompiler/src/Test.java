8aa44ab55c1f6cbdc5e444fca908b2febecbda4f
Earthcomputer <burtonjae@hotmail.co.uk>
2016-01-29 13:33:05 +0000

    JavaTokenizer

--- JavaCompiler/src/Test.java
+++ JavaCompiler/src/Test.java
@@ -1,13 +1,10 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
 
 import javax.swing.JFileChooser;
 
 import net.earthcomputer.compiler.JavaCompilerExt;
-import net.earthcomputer.compiler.internal.UnicodeReplacerReader;
 
 public class Test {
 
@@ -15,11 +12,6 @@ public class Test {
 		JFileChooser chooser = new JFileChooser();
 		chooser.showOpenDialog(null);
 		JavaCompilerExt.compile(new BufferedReader(new FileReader(chooser.getSelectedFile())));
-		
-		Scanner scanner = new Scanner(new UnicodeReplacerReader(new InputStreamReader(System.in)));
-		while(true) {
-			System.out.println(scanner.nextLine());
-		}
 	}
 
 }
--- JavaCompiler/src/net/earthcomputer/compiler/JavaCompilerExt.java
+++ JavaCompiler/src/net/earthcomputer/compiler/JavaCompilerExt.java
@@ -2,8 +2,8 @@ package net.earthcomputer.compiler;
 
 import java.io.IOException;
 import java.io.Reader;
-import java.io.StreamTokenizer;
 
+import net.earthcomputer.compiler.internal.JavaTokenizer;
 import net.earthcomputer.compiler.internal.UnicodeReplacerReader;
 
 public class JavaCompilerExt {
@@ -13,41 +13,11 @@ public class JavaCompilerExt {
 	}
 
 	private static byte[] compileWithoutUnicode(Reader input) throws IOException {
-		StreamTokenizer javaTokenizer = new StreamTokenizer(input);
-		javaTokenizer.resetSyntax();
-		javaTokenizer.whitespaceChars(0, ' ');
-		javaTokenizer.wordChars('0', '9');
-		javaTokenizer.wordChars('A', 'Z');
-		javaTokenizer.wordChars('a', 'z');
-		javaTokenizer.wordChars('_', '_');
-		javaTokenizer.wordChars('$', '$');
-		javaTokenizer.quoteChar('\'');
-		javaTokenizer.quoteChar('"');
-		javaTokenizer.slashSlashComments(true);
-		javaTokenizer.slashStarComments(true);
-
-		int token;
-		while ((token = javaTokenizer.nextToken()) != StreamTokenizer.TT_EOF) {
-			switch (token) {
-			case StreamTokenizer.TT_EOL:
-				System.out.println("New line");
-				break;
-			case StreamTokenizer.TT_NUMBER:
-				System.out.println("Number: " + javaTokenizer.nval);
-				break;
-			case StreamTokenizer.TT_WORD:
-				System.out.println("Word: " + javaTokenizer.sval);
-				break;
-			default:
-				if (javaTokenizer.sval == null) {
-					System.out.println("Token: " + (char) token);
-				} else {
-					System.out.println("Quoted: " + (char) token + javaTokenizer.sval + (char) token);
-				}
-			}
-		}
+		JavaTokenizer tokenizer = new JavaTokenizer(input);
 		
-		System.out.println("\730");
+		while (tokenizer.nextToken()) {
+			System.out.println("<" + tokenizer.sval + ">");
+		}
 		
 		return null;
 	}
